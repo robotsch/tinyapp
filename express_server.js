0106911c573ae835f90b2eee2de86c91a92e56fd
robotsch <christianhumble22@gmail.com>
2022-03-21 23:59:16 -0400

    add new url template, add redirect

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,20 +11,41 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const genStr = function generateRandomString() {
+  const rndStr = '0123456789abcdefABCDEF'
+  let result = ''
+  for(let i = 0; i < 6; i += 1) {
+    // Thank you once again, StackOverflow
+    result += rndStr[Math.floor(Math.random()* rndStr.length)]
+  }
+  return result
+}
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  res.redirect(urlDatabase[req.params.shortURL])
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+  // console.log(genStr())
 });
