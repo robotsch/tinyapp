2f2f33b63dfdd26d9375faa19a9d6c546081e209
robotsch <christianhumble22@gmail.com>
2022-03-22 10:55:07 -0400

    add delete functionality

--- express_server.js
+++ express_server.js
@@ -27,6 +27,11 @@ app.post("/urls", (req, res) => {
   res.send("Ok");         
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls");         
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
